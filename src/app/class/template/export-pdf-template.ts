d503b3b6a8a1c84cbab2e5bcb1b239386beb290e
Dante <danteadonai2602@outlook.com>
2025-07-04 16:34:18 -0700

    Update template for XLSX export

---
+++ src/app/class/template/export-pdf-template.ts
@@ -0,0 +1,17 @@
+import { jsPDF } from 'jspdf';
+export abstract class ExportPdfTemplate {
+  abstract name: string;
+  abstract orientation: 'portrait' | 'landscape';
+  abstract unit: 'pt' | 'mm' | 'cm' | 'in';
+  abstract format: [number, number];
+  export() {
+    const doc = new jsPDF({
+      orientation: this.orientation,
+      unit: this.unit,
+      format: this.format,
+    });
+    this.applyFormatting(doc);
+    doc.save(`${this.name}.pdf`);
+  }
+  abstract applyFormatting(doc: jsPDF): void;
+}
--- src/app/service/file.service.ts
+++ src/app/service/file.service.ts
@@ -12,6 +12,8 @@ import { XLSXMatrixBuilder } from '../class/builder/xlsx-matrix-builder';
 import { XLSXFormattingBuilder } from '../class/builder/xlsx-formatting-builder';
 import { XLSXStylingStrategy } from '../class/strategy/xlsx-styling-strategy';
 import { XLSXStyleDirector } from '../class/director/xlsx-style-director';
+import { MovementsExportXLSXTemplate } from '../class/template/movements-export-xlsx-template';
+import { MovementsExportPdfTemplate } from '../class/template/movements-export-pdf-template';
 
 @Injectable({
   providedIn: 'root',
@@ -27,78 +29,10 @@ export class FileService {
     a.click();
     window.URL.revokeObjectURL(url);
   }
-
-  exportTableToXLSX(tableMovements: TableMovement[], tableBalance: TableBalance[]) {
-    const workbook = XLSX.utils.book_new();
-    // Convert the table data to the required format for XLSX
-    const movements = FileAdapter.toXLSXMovements(tableMovements);
-    const balances = FileAdapter.toXLSXBalances(tableBalance);
-    // transform the data into a structured format
-    const data = this.createStructuredData(movements, balances);
-    // Create a worksheet from the structured data as an array of arrays
-    const worksheet = XLSX.utils.aoa_to_sheet(data);
-    // Apply formatting and styling to the worksheet
-    this.applyWorksheetFormatting(worksheet, data[0].length, data.length);
-    this.applyWorksheetStyling(worksheet, data[0].length, data.length);
-    // Set the worksheet name and append it to the workbook
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Movements');
-    XLSX.writeFile(workbook, 'movements.xlsx');
+  exportTableMovementsToXLSX(tableMovements: TableMovement[], tableBalance: TableBalance[]) {
+    new MovementsExportXLSXTemplate(tableMovements, tableBalance).export();
   }
-
-  private createStructuredData(tableMovements: JSONMovementXLSX[], tableBalance: JSONBalanceXLSX[]): any[] {
-    const builder = new XLSXMatrixBuilder();
-    builder.setHeaders(['', '', 'ITEM', '', '', 'VALUES', '', '', '', '']);
-    builder.setHeaders(['DATE', 'CONCEPT', 'ENTRIES', 'EXITS', 'STOCK', 'ACQUISITION', 'DEBIT', 'CREDIT', 'BALANCE', 'CREATED BY']);
-    tableMovements.forEach(movement => {
-      builder.setRow(Object.values(movement));
-    })
-    builder.setSpacing();
-    builder.setHeaders(['AVAILABLE STOCK', 'UNIT PRICE', 'FINAL BALANCE']);
-    tableBalance.forEach(movement => {
-      builder.setRow(Object.values(movement));
-    })
-    return builder.getMatrix();
-  }
-
-  private applyWorksheetFormatting(worksheet: XLSX.WorkSheet, xLength: number, yLength: number): void {
-    const builder = new XLSXFormattingBuilder();
-    builder.setRange(0, 0, yLength - 1, xLength - 1);
-    builder.setMergedCell(0, 2, 0, 4);
-    builder.setMergedCell(0, 5, 0, 8);
-    for(let i = 0; i < xLength; i++){
-      builder.setColumnWidth(20);
-    }
-    const config = builder.getConfig();
-    // Define range
-    worksheet['!ref'] = XLSX.utils.encode_range(config.range!);
-    // Merge cells for main headers
-    worksheet['!merges'] = config.mergedCells;
-    // Configure column widths
-    worksheet['!cols'] = config.columnWidths;
-  }
-
-  private applyWorksheetStyling(worksheet: XLSX.WorkSheet, xLength: number, yLength: number): void {
-    // Create styles for headers and sub-headers
-    const director = new XLSXStyleDirector();
-    const headerStyle = director.getHeaderStyle();
-    const subHeaderStyle = director.getSubHeaderStyle();
-    const dataRowStyle = director.getDataRowStyle();
-    const strategy = new XLSXStylingStrategy(dataRowStyle);
-    for(let row = 0; row < yLength; row++){
-      this.applyStyleRow(worksheet, strategy, row, xLength);
-    }
-    strategy.setStyling(headerStyle);
-    this.applyStyleRow(worksheet, strategy, 0, xLength);
-    strategy.setStyling(subHeaderStyle);
-    this.applyStyleRow(worksheet, strategy, 1, xLength);
-  }
-  private applyStyleRow(Worksheet: XLSX.WorkSheet, strategy: XLSXStylingStrategy, numRow: number, nColumns: number): void{
-    for(let col = 0; col <= nColumns; col++){
-      const cellAddress = XLSX.utils.encode_cell({c: col, r: numRow});
-      if(!Worksheet[cellAddress]){
-        Worksheet[cellAddress] = {};
-      }
-      strategy.applyStyle(Worksheet[cellAddress]);
-    }
+  exportTableMovementsToPDF(tableMovements: TableMovement[], tableBalance: TableBalance[]) {
+    new MovementsExportPdfTemplate(tableMovements, tableBalance).export();
   }
 }
