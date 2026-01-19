a1e66ef97c62b28d0723ad12d98424f874a4ee2a
Dante <danteadonai2602@outlook.com>
2026-01-19 14:11:26 -0700

    build: Se actualiza los env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API: 'https://inventory-card.onrender.com/api'
+  API: 'https://web-rjja989auu0t.up-de-fra1-k8s-1.apps.run-on-seenode.com/api'
 };
