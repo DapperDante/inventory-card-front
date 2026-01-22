e1d08957a2c458fd30103d80224a3981f26dd2d6
Dante <danteadonai2602@outlook.com>
2026-01-22 09:40:49 -0700

    build: Se actualiza los env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API: 'https://web-rjja989auu0t.up-de-fra1-k8s-1.apps.run-on-seenode.com/api'
+  API: 'https://inventory-card-production.up.railway.app/api'
 };
